a81fae9c10917319ef78f20052ccf23c923368b7
aakritytuladhar <tuladhar.aakriti@gmail.com>
2024-02-22 11:10:18 +0545

    new

--- screens/PickUpScreen.js
+++ screens/PickUpScreen.js
@@ -1,8 +1,13 @@
 import { StyleSheet, Text, View, TextInput } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import HorizontalDatepicker from "@awrminkhodaei/react-native-horizontal-datepicker";
 
 const PickUpScreen = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date().toISOString());
+  const nextMonth = new Date();
+  nextMonth.setMonth(nextMonth.getMonth() + 1);
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ backgroundColor: "#F0F0F0", flex: 1 }}>
@@ -25,6 +30,22 @@ const PickUpScreen = () => {
             style={{ fontSize: 16, fontWeight: "500", marginHorizontal: 10 }}>
             Pick Up Date
           </Text>
+          <HorizontalDatepicker
+            mode="gregorian"
+            startDate={new Date()}
+            endDate={nextMonth}
+            initialSelectedDate={new Date()}
+            onSelectedDateChange={(date) => setSelectedDate(date.toISOString())}
+            selectedItemWidth={170}
+            unselectedItemWidth={38}
+            itemHeight={38}
+            itemRadius={10}
+            selectedItemTextStyle={styles.selectedItemTextStyle}
+            unselectedItemTextStyle={styles.selectedItemTextStyle}
+            selectedItemBackgroundColor="#222831"
+            unselectedItemBackgroundColor="#ececec"
+            flatListContainerStyle={styles.flatListContainerStyle}
+          />
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
